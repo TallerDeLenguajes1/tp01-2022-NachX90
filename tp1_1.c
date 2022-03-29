3a566f158fa86cac1d85bb4e1f5167ada11aa2c7
NachX90 <nachx90@protonmail.com>
2022-03-29 00:05:48 -0300

    variables added

--- tp1_1.c
+++ tp1_1.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
 void main(){
-    printf("Hola Mundo");
+    printf("Hola Mundo\n");
+    int year, *p_year;
+    year = 2022;
+    p_year = &year;
+    printf("Contenido del puntero: %i\n", *p_year);
+    printf("Direccion de memoria almacenada por el puntero: %p\n", p_year);
+    printf("Direccion de memoria de la variable: %p\n", &year);
+    printf("Direccion de memoria del puntero: %p\n", &p_year);
+    printf("Tamanio de memoria utilizado por la variable: %d bytes", sizeof(year));
 }
